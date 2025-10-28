45784273e8bc078c42d5fcb0747cf61b154acea9
WillmanGZ <willmang@uninorte.edu.co>
2025-10-28 11:15:30 -0500

    feat: DB Connection

---
+++ AdminManager.Web/Configs/DatabaseConfig.cs
@@ -0,0 +1,26 @@
+﻿using AdminManager.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminManager.Web.Configs
+{
+    public static class DatabaseConfig
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            DotNetEnv.Env.Load();
+
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var port = Environment.GetEnvironmentVariable("DB_PORT");
+            var name = Environment.GetEnvironmentVariable("DB_NAME");
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var pass = Environment.GetEnvironmentVariable("DB_PASS");
+
+            var connectionString = $"Host={host};Port={port};Database={name};Username={user};Password={pass}";
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(connectionString));
+
+            return services;
+        }
+    }
+}
---
+++ AdminManager.Web/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AdminManager.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminManager.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
