63552c3e8a9865dc6fb82f32f6d6c508c74a956a
WillmanGZ <willmang@uninorte.edu.co>
2025-10-29 19:07:25 -0500

    feat: aplicar el identity en el program y actualizacion de los paquetes

--- AdminManager.Web/Program.cs
+++ AdminManager.Web/Program.cs
@@ -1,25 +1,27 @@
 using AdminManager.Web.Configs;
 using AdminManager.Web.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDatabase();
+
+// Use identity user system and role system
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>() // Using our dbContext
+    .AddDefaultTokenProviders() // Using tokens for reset or confirmation
+    .AddDefaultUI(); // Use razor pages
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
-// Apply migrations when app starts
-using var scope = app.Services.CreateScope();
-var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-await db.Database.MigrateAsync();
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
