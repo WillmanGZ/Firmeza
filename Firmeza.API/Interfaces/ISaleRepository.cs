419010069bff12229d371d368a9c117b631f627c
WillmanGZ <willmang@uninorte.edu.co>
2025-11-05 19:53:50 -0500

    feat: sale repository

---
+++ Firmeza.API/Interfaces/ISaleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Firmeza.API.Data.Entities;
+
+namespace Firmeza.API.Interfaces
+{
+    public interface ISaleRepository
+    {
+        Task<List<Sale>> GetAllAsync();
+        Task<Sale?> GetByIdAsync(Guid id);
+        Task AddAsync(Sale sale);
+        Task UpdateAsync(Sale sale);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ Firmeza.API/Repositories/SaleRepository.cs
@@ -0,0 +1,49 @@
+﻿using Firmeza.API.Data;
+using Firmeza.API.Data.Entities;
+using Firmeza.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Firmeza.API.Repositories
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SaleRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public async Task<List<Sale>> GetAllAsync()
+        {
+            return await _context.Sales.ToListAsync();
+        }
+
+        public async Task<Sale?> GetByIdAsync(Guid id)
+        {
+            return await _context.Sales.FindAsync(id);
+        }
+
+        public async Task AddAsync(Sale sale)
+        {
+            _context.Sales.Add(sale);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Sale sale)
+        {
+            _context.Sales.Update(sale);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var sale = await _context.Sales.FindAsync(id);
+            if (sale != null)
+            {
+                _context.Sales.Remove(sale);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
